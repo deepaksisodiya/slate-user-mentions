0d86a8f6914aab9add2c2df2ebf949ca77e82ff2
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2021-06-07 14:27:59 +0530

    doing code for mention user

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, {  useMemo, useState } from 'react';
-
 import { createEditor } from 'slate';
-
 import { Slate, Editable, withReact } from 'slate-react'
+import { withHistory } from 'slate-history'
+
+const initialValue = [
+  {
+    type: 'paragraph',
+    children: [{ text: 'A line of text in a paragraph.' }],
+  },
+];
 
 const App = () => {
 
-  const editor = useMemo(() => withReact(createEditor()), []);
+  const editor = useMemo(() => withMentions(withReact(withHistory(createEditor()))), []);
 
-  const [value, setValue] = useState([
-    {
-      type: 'paragraph',
-      children: [{ text: 'A line of text in a paragraph.' }],
-    },
-  ]);
+  const [value, setValue] = useState(initialValue);
 
   return (
     <div style={{ border: '1px solid' }}>
@@ -27,6 +28,23 @@ const App = () => {
     </div>
     
   );
+}
+
+// plugins
+// here we are defining that mention is inline element
+// and mention is also void element, means we can not edit it
+const withMentions = editor => {
+  const { isInline, isVoid } = editor
+
+  editor.isInline = element => {
+    return element.type === 'mention' ? true : isInline(element)
+  }
+
+  editor.isVoid = element => {
+    return element.type === 'mention' ? true : isVoid(element)
+  }
+
+  return editor
 }
 
 export default App;
