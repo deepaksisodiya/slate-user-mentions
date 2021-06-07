540ed11d05bdc40c0f0ff054f212bc9b19b14e0c
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2021-06-07 14:18:11 +0530

    slate init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import './App.css';
+import React, {  useMemo, useState } from 'react';
 
-function App() {
+import { createEditor } from 'slate';
+
+import { Slate, Editable, withReact } from 'slate-react'
+
+const App = () => {
+
+  const editor = useMemo(() => withReact(createEditor()), []);
+
+  const [value, setValue] = useState([
+    {
+      type: 'paragraph',
+      children: [{ text: 'A line of text in a paragraph.' }],
+    },
+  ]);
+
   return (
-    <div className="App">
-      <div>Slate.js</div>
+    <div style={{ border: '1px solid' }}>
+      <Slate
+        editor={editor}
+        value={value}
+        onChange={newValue => setValue(newValue)}
+      >
+        <Editable />
+      </Slate>
     </div>
+    
   );
 }
 
